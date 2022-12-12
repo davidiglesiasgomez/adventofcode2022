23b21b5b3c3577b5ba85d8ae7c9ed8a3311c672e
davidiglesiasgomez <david.iglesias@gmail.com>
2022-12-13 00:14:58 +0100

    Day11 task2

--- day11.php
+++ day11.php
@@ -4,15 +4,14 @@ include_once('vendor/autoload.php');
 
 $file = file_get_contents('input/input.day11.txt');
 
-$day11Obj = new \AdventOfCode2022\Day11($file);
+$day11Obj = new \AdventOfCode2022\Day11($file, 1);
 for ($i=1; $i<=20; $i++) {
     $day11Obj->ejecutarRonda();
 }
 echo 'Work ' . $day11Obj->comprobarTrabajoTopDos() . PHP_EOL;
 
-$day11Obj = new \AdventOfCode2022\Day11($file);
+$day11Obj = new \AdventOfCode2022\Day11($file, 2);
 for ($i=1; $i<=10000; $i++) {
-    echo 'Round ' . $i . '/10000' . PHP_EOL;
-    $day11Obj->ejecutarRonda(1);
+    $day11Obj->ejecutarRonda();
 }
 echo 'Work ' . $day11Obj->comprobarTrabajoTopDos() . PHP_EOL;
